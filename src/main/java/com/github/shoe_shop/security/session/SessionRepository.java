41d7d45cf3dae4be5ce69cc10729fa15f723971f
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-04-06 17:00:11 +0300

    add method to find active session by ip

--- src/main/java/com/github/shoe_shop/security/session/SessionRepository.java
+++ src/main/java/com/github/shoe_shop/security/session/SessionRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Repository
 public interface SessionRepository extends CrudRepository<Session, Long> {
@@ -14,4 +15,10 @@ public interface SessionRepository extends CrudRepository<Session, Long> {
                 delete from Session s where s.lastPerformedActionDate < ?1
             """)
     Long deleteExpiredSessions(final LocalDateTime expirationDate);
+
+    @Query(value = """
+                select s from Session s where s.workstation.ip = ?1 and s.lastPerformedActionDate > ?2 
+                order by s.lastPerformedActionDate desc limit 1
+            """)
+    Optional<Session> findActiveSessionByIP(final String ip, final LocalDateTime expirationDate);
 }
