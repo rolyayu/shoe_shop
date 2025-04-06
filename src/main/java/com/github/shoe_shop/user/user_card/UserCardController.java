6fa08b64682b63cedef0c7ed386ca2ad43421891
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-04-06 19:09:03 +0300

    create user card controller

---
+++ src/main/java/com/github/shoe_shop/user/user_card/UserCardController.java
@@ -0,0 +1,25 @@
+package com.github.shoe_shop.user.user_card;
+
+import com.github.shoe_shop.user.user_card.dto.CreateCardDto;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user_cards")
+@Secured("ADMINISTRATOR")
+public class UserCardController {
+    private final UserCardDtoMapper dtoMapper;
+
+    private final UserCardService cardService;
+
+    @PostMapping
+    public UserCard createCard(@Valid @RequestBody final CreateCardDto createCardDto) {
+        return cardService.createCard(dtoMapper.mapToEntity(createCardDto));
+    }
+}
