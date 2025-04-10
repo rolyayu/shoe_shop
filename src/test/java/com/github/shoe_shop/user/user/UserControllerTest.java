c47f59d5a144b9f491bf62c75b16ffcf68552a6f
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-04-10 18:01:00 +0300

    add test for non admin case

--- src/test/java/com/github/shoe_shop/user/user/UserControllerTest.java
+++ src/test/java/com/github/shoe_shop/user/user/UserControllerTest.java
@@ -1,27 +1,26 @@
 package com.github.shoe_shop.user.user;
 
-import com.github.shoe_shop.security.config.AuthenticationManagerConfig;
-import com.github.shoe_shop.security.config.FilterConfig;
-import com.github.shoe_shop.security.config.SecurityConfig;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.github.shoe_shop.user.user.dto.CreateUserDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Import;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc()
 class UserControllerTest {
 
     @Autowired
@@ -41,11 +40,49 @@ class UserControllerTest {
     @BeforeEach
     void setUpUser() {
         user = new User();
+        user.setUsername("administrator");
+        user.setEncodedPassword("password");
+        user.setRole(UserRole.ADMINISTRATOR);
     }
 
     @Test
-    @WithMockUser(roles = {"ADMINISTRATOR"})
-    void createUser() {
-        when(encoder.encode("admin")).thenReturn("admin");
+    @WithMockUser(authorities = {"ADMINISTRATOR"})
+    void createUser_returnOkAndCreatedUser() throws Exception {
+        final CreateUserDto createUserDto = new CreateUserDto(
+                user.getUsername(),
+                user.getEncodedPassword(),
+                UserRole.CASHIER.name()
+        );
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final ObjectWriter objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
+        final String mappedUser = objectWriter.writeValueAsString(createUserDto);
+
+        when(encoder.encode(user.getEncodedPassword())).thenReturn(user.getEncodedPassword());
+        when(userDtoMapper.createDtoToUser(createUserDto)).thenReturn(user);
+        when(userService.createUser(user)).thenReturn(user);
+
+        mvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mappedUser))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value(user.getUsername()));
+    }
+
+    @Test
+    @WithMockUser(authorities = {"CASHIER"})
+    void createUser_returnForbiddenForNonAdminUser() throws Exception {
+        final CreateUserDto createUserDto = new CreateUserDto(
+                user.getUsername(),
+                user.getEncodedPassword(),
+                UserRole.CASHIER.name()
+        );
+        final ObjectMapper objectMapper = new ObjectMapper();
+        final ObjectWriter objectWriter = objectMapper.writer().withDefaultPrettyPrinter();
+        final String mappedUser = objectWriter.writeValueAsString(createUserDto);
+
+        mvc.perform(post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mappedUser))
+                .andExpect(status().isForbidden());
     }
 }
