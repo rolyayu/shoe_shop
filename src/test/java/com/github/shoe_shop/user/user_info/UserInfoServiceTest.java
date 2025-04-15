781eba4b2f893a4f52f788962d8f90b1bdced592
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-04-15 14:15:23 +0300

    create integrated test instead if unit for UserInfoService.CreateInfo

--- src/test/java/com/github/shoe_shop/user/user_info/UserInfoServiceTest.java
+++ src/test/java/com/github/shoe_shop/user/user_info/UserInfoServiceTest.java
@@ -4,44 +4,34 @@ import com.github.shoe_shop.exceptions.EntityAlreadyExistsException;
 import com.github.shoe_shop.user.user.User;
 import com.github.shoe_shop.user.user.UserRepository;
 import com.github.shoe_shop.user.user.UserRole;
-import com.github.shoe_shop.user.user.UserService;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import shared.BaseContainerTest;
 
 import java.time.LocalDate;
-import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-class UserInfoServiceTest  {
+@SpringBootTest
+class UserInfoServiceTest extends BaseContainerTest {
     private static User user;
 
     private static UserInfo userInfo;
 
-    @InjectMocks
+    @Autowired
     private UserInfoService userInfoService;
 
-    @Mock
-    private UserService userService;
-
-    @Mock
+    @Autowired
     private UserInfoRepository userInfoRepository;
 
-    @BeforeEach
-    void setUpMocks() {
-        MockitoAnnotations.openMocks(this);
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     @BeforeAll
     static void setUpUsers() {
@@ -52,28 +42,38 @@ class UserInfoServiceTest  {
         user.setRole(UserRole.ORGANIZATION_OWNER);
 
         userInfo = new UserInfo();
-        userInfo.setBirthDate(LocalDate.of(2003,1,27));
+        userInfo.setBirthDate(LocalDate.of(2003, 1, 27));
         userInfo.setFullName("Shoe Shop");
         userInfo.setGender(Gender.MALE);
     }
 
+    @BeforeEach
+    @Transactional
+    void setUpDB() {
+        userInfoRepository.deleteAll();
+        userRepository.deleteAll();
+        userInfo.setUser(null);
+        userInfo.setId(null);
+        user.setId(null);
+    }
+
     @Test
     public void createInfo_returnCreatedInfo_whenDoesntExistForUser() {
-        when(userService.findById(Mockito.anyLong())).thenReturn(user);
-        when(userInfoRepository.findByUser(user)).thenReturn(Optional.empty());
-        when(userInfoRepository.save(userInfo)).thenReturn(userInfo);
+        final User createdUser = userRepository.save(user);
 
-        final UserInfo savedUserInfo = userInfoService.createInfo(userInfo, user.getId());
+        final UserInfo savedUserInfo = userInfoService.createInfo(userInfo, createdUser.getId());
 
         assertNotNull(savedUserInfo);
         assertEquals(savedUserInfo, userInfo);
         assertNotNull(savedUserInfo.getUser());
+        assertNotNull(savedUserInfo.getId());
     }
 
     @Test
     public void createInfo_throwAlreadyExists_whenUserHasItsInfo() {
-        when(userService.findById(Mockito.anyLong())).thenReturn(user);
-        when(userInfoRepository.findByUser(user)).thenReturn(Optional.of(userInfo));
+        final User createdUser = userRepository.save(user);
+        userInfo.setUser(createdUser);
+        userInfoRepository.save(userInfo);
 
         assertThrows(EntityAlreadyExistsException.class, () -> userInfoService.createInfo(userInfo, user.getId()));
     }
