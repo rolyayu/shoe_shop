6beda49ac31d1484b1c52d0e0f7911a945b6ba00
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-04-19 10:13:54 +0300

    extend AuthenticationException and give base message

--- src/main/java/com/github/shoe_shop/exceptions/IncorrectCredentialsException.java
+++ src/main/java/com/github/shoe_shop/exceptions/IncorrectCredentialsException.java
@@ -1,10 +1,14 @@
 package com.github.shoe_shop.exceptions;
 
-import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
 
-public class IncorrectCredentialsException extends BadCredentialsException {
+public class IncorrectCredentialsException extends AuthenticationException {
     public IncorrectCredentialsException(String msg) {
         super(msg);
+    }
+
+    public IncorrectCredentialsException() {
+        super("Password or username is incorrect");
     }
 
     public IncorrectCredentialsException(String msg, Throwable cause) {
