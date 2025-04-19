9de2f2fcf0ef7712bc07596f1bbd3914b5081153
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-04-19 10:15:58 +0300

    catch own exceptions in http layer

--- src/main/java/com/github/shoe_shop/web/advices/GlobalAdviceController.java
+++ src/main/java/com/github/shoe_shop/web/advices/GlobalAdviceController.java
@@ -1,6 +1,8 @@
 package com.github.shoe_shop.web.advices;
 
+import com.github.shoe_shop.exceptions.BadArgumentsException;
 import com.github.shoe_shop.exceptions.EntityAlreadyExistsException;
+import com.github.shoe_shop.exceptions.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,16 +20,18 @@ public class GlobalAdviceController {
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleGlobalException(final Exception e) {
         if (e instanceof EntityAlreadyExistsException entityAlreadyExistsException) {
-            return ResponseEntity.badRequest().body("entityAlreadyExistsException handled with message: "
-                    + entityAlreadyExistsException.getMessage());
-        } else if(e instanceof AccessDeniedException accessDeniedException) {
+            return ResponseEntity.badRequest().body("Entity you're trying to create already exists." + e.getMessage());
+        } else if (e instanceof EntityNotFoundException entityNotFoundException) {
+            return new ResponseEntity<>("Entity you're looking for is not found." + e.getMessage(), HttpStatus.NOT_FOUND);
+        } else if (e instanceof BadArgumentsException badArgumentsException) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        } else if (e instanceof AccessDeniedException accessDeniedException) {
             return new ResponseEntity<>(accessDeniedException.getMessage(), HttpStatus.FORBIDDEN);
         } else if (e instanceof AuthenticationException authenticationException) {
             return new ResponseEntity<>(authenticationException.getMessage(), HttpStatus.UNAUTHORIZED);
-        } else if(e instanceof HttpMessageNotReadableException httpMessageNotReadableException) {
+        } else if (e instanceof HttpMessageNotReadableException httpMessageNotReadableException) {
             return new ResponseEntity<>(httpMessageNotReadableException.getMessage(), HttpStatus.BAD_REQUEST);
-        }
-        else {
+        } else {
             return ResponseEntity.internalServerError().body("Unknown exception with message: " + e.getMessage());
         }
     }
