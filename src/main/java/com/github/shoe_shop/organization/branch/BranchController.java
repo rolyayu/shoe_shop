73af7a3b9f17305441e71e0ced87224b28c274ea
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-04-25 08:42:03 +0300

    make createBranch return CREATED status

--- src/main/java/com/github/shoe_shop/organization/branch/BranchController.java
+++ src/main/java/com/github/shoe_shop/organization/branch/BranchController.java
@@ -5,11 +5,13 @@ import com.github.shoe_shop.organization.branch.dto.CreateBranchDto;
 import com.github.shoe_shop.web.annotations.OrganizationOwnerValidUNP;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,8 +22,9 @@ public class BranchController {
     private final BranchDtoMapper branchDtoMapper;
 
     @PostMapping
-    @Secured({"ORGANIZATION_OWNER", "ADMINISTRATOR"})
     @OrganizationOwnerValidUNP
+    @ResponseStatus(HttpStatus.CREATED)
+    @Secured({"ORGANIZATION_OWNER", "ADMINISTRATOR"})
     public Branch createBranch(@Valid @UNP @PathVariable final String unp, @Valid @RequestBody final CreateBranchDto createBranchDto) {
         final Branch branchToCreate = branchDtoMapper.mapCreateDtoToEntity(createBranchDto);
         return branchService.createBranch(branchToCreate, unp);
