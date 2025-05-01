b24002d271161e73b6e5c8117c8989b48ab4ec74
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-05-01 19:17:20 +0300

    add response status and change endpoints uri

--- src/main/java/com/github/shoe_shop/organization/workstations/WorkstationController.java
+++ src/main/java/com/github/shoe_shop/organization/workstations/WorkstationController.java
@@ -1,16 +1,16 @@
 package com.github.shoe_shop.organization.workstations;
 
 import com.github.shoe_shop.base.constraints.IP;
-import com.github.shoe_shop.organization.workstations.dto.CreateWorkstationDto;
 import com.github.shoe_shop.web.annotations.RequestIP;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.hibernate.validator.constraints.UUID;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,16 +21,18 @@ public class WorkstationController {
 
     private final WorkstationService service;
 
-    private final WorkstationDtoMapper dtoMapper;
 
-
-    @PostMapping
-    public Workstation createWorkstation(@RequestBody @Valid final CreateWorkstationDto createDto) {
-        return service.create(dtoMapper.mapToEntity(createDto), createDto.branchId());
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/ip/{ip}/branch/{branch_id}")
+    public Workstation createWorkstation(@PathVariable @Valid @IP String ip, @PathVariable("branch_id") @Valid @IP String branchId) {
+        final Workstation workstationToCreate = new Workstation();
+        workstationToCreate.setIp(ip);
+        return service.create(workstationToCreate, branchId);
     }
 
-    @PostMapping("/current/branch/{branch_id}")
-    public Workstation createWorkstationByRequest(@RequestIP @IP final String ip,
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping("/current_ip/branch/{branch_id}")
+    public Workstation createWorkstationByRequest(@Valid @RequestIP @IP final String ip,
                                                   @Valid @PathVariable("branch_id") @UUID final String branchId) {
         final Workstation workstationToCreate = new Workstation();
         workstationToCreate.setIp(ip);
