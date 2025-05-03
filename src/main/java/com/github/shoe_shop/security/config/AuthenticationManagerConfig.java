de9d7c8642ccc980f777be525bee634817854dbf
Anton Rudkovsky <a.rudkovsky@lacit.net>
2025-05-03 07:08:46 +0300

    remove LoginAuthenticationProvider usage

--- src/main/java/com/github/shoe_shop/security/config/AuthenticationManagerConfig.java
+++ src/main/java/com/github/shoe_shop/security/config/AuthenticationManagerConfig.java
@@ -1,7 +1,6 @@
 package com.github.shoe_shop.security.config;
 
 import com.github.shoe_shop.security.jwt.JwtTokenProvider;
-import com.github.shoe_shop.security.login.LoginAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,14 +15,11 @@ public class AuthenticationManagerConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
 
-    private final LoginAuthenticationProvider loginAuthenticationProvider;
-
     @Bean
     @Primary
     public AuthenticationManager myAuthBuilder(final ObjectPostProcessor<Object> objectObjectPostProcessor) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = new AuthenticationManagerBuilder(objectObjectPostProcessor);
         authenticationManagerBuilder.authenticationProvider(jwtTokenProvider);
-        authenticationManagerBuilder.authenticationProvider(loginAuthenticationProvider);
         return  authenticationManagerBuilder.build();
     }
 }
